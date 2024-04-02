9e8733c3505830bdca5969a7d6b0939eca20145a
tqt97 <kutuanonline199@gmail.com>
2024-04-02 09:21:58 +0700

    Saving a Model:  or

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,11 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // that strictness should only be present in a local environment.
+        Model::shouldBeStrict(! app()->isProduction());
+
+        // You don't need to add the $guarded property in every Model by providing unguard() in the Provider.
+        Model::unguard();
+
     }
 }
